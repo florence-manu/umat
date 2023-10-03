d1d10844e32ed9b4fe24617e2f2df3618a6c0784
Ebenezer Arkoh-Amoah <arkohamoah@gmail.com>
2023-10-04 06:09:48 +1200

    completed the institiution page

--- src/components/studentApplication/education/institutions/index.tsx
+++ src/components/studentApplication/education/institutions/index.tsx
@@ -1,19 +1,177 @@
 import React from "react";
 import styled from "styled-components";
+import { fieldset1 } from "../../../../utils/strings/education/institiutions";
+import { fieldset2 } from "../../../../utils/strings/education/institiutions";
+import { fieldset3 } from "../../../../utils/strings/education/institiutions";
+import { fieldset4 } from "../../../../utils/strings/education/institiutions";
+import { fieldset5 } from "../../../../utils/strings/education/institiutions";
+import { MapInputs } from "../../../../utils/functions/map";
+import { Button } from "../../../button";
+import edit from "../../../../images/edit.png";
+import Delete from "../../../../images/Delete.png";
+import gh from "../../../../images/ghanaFlag.png";
+import uk from "../../../../images/ukFlag.png";
+import { Buttons } from "../../navButtons";
 
-export const Container = styled.div`
-width: 90%;
+
+export interface Background {
+    school: string;
+    flag: string;
+    status: string;
+}
+
+export const RecordsContainer = styled.div`
 display: flex;
 flex-direction: row;
-justify-content: space-between
+align-items: center;
+gap: 15px;
+
+width: 100%;
+
+p{
+    font-size: 15px;
+}
+
+.school{
+    width: 180%;
+}
+
+    img{
+    width: 25px;
+    height: 25px;
+   }
+`
+
+export function Record({school, status, flag}: Background) {
+    
+    return (
+        <RecordsContainer>
+            <img src={flag} alt="flag" />
+            <p className="school">{school}</p>
+            <p>{status}</p>
+            <img src={edit} alt="edit" />
+            <img src={Delete} alt="delete" />
+        </RecordsContainer>
+    )
+}
+
+export const Container = styled.div`
+width: 100%;
+display: flex;
+flex-direction: row;
+gap: 5%;
+`
+
+export const InputsWrapper = styled.div`
+display: flex;
+flex-direction: column;
+gap: 30px;
+`
+
+export const Inputs = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 10px;
+`
+
+export const Section = styled.div`
+ width: 100%;
+ display: flex;
+ flex-direction: row;
+justify-content: space-between;
+` 
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`
+export const RecordsContaner = styled.div`
+width: 95%;
+display: flex;
+align-items: flex-start;
+justify-content: flex-end;
+gap: 120px;
+flex-direction: column;
+`
+
+export const Records = styled.div`
+  background-color: #FCFCFC;
+    border-radius: 10px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 50px 30px ;
+    text-align: center;
+    gap: 40px;
+    box-shadow: 2px 3px 5px 5px #737373;
+
+p{
+    color: #4D4D4D;
+}
+
+ul{
+    list-style: none;
+    li{
+        margin-bottom: 20px;
+    }
+}
 `
 
 export function Institutions() {
+     const Nationality = {
+    content: fieldset1,
+  };
+     const Completion = {
+    content: fieldset2,
+  };
+     const institiution = {
+    content: fieldset3,
+  };
+     const level = {
+    content: fieldset4,
+  };
+     const status = {
+    content: fieldset5,
+    };
     
     return (
         <Container>
-            <div>Hello Iam one</div>
-            <div>Hello Iam two</div>
+            <InputsWrapper>
+                <Section>
+                    {MapInputs(Nationality)}
+                  </Section>
+                <Inputs>
+                     <Section>
+                    {MapInputs(Completion)}
+                </Section>
+                     <Section>
+                    {MapInputs(institiution)}
+                </Section>
+                     <Section>
+                    {MapInputs(level)}
+                </Section>
+                     <Section>
+                        {MapInputs(status)}
+                        <ButtonsContainer>
+                            <Button color="#24292F" text="Cancel" width={100} background="#F6F8FA" />
+                            <Button color="white" text="Save" width={100} background="#2DA44E" />
+                       </ButtonsContainer>
+                </Section>
+                </Inputs>
+            </InputsWrapper>
+            <RecordsContaner>
+                <Records>
+                    <p><b>Educational Records</b></p>
+                    <ul>
+                        <li><Record flag={gh} school="Kings University College" status="stopped"/></li>
+                        <li><Record flag={uk} school="Holy Home International" status="completed"/></li>
+                        <li><Record flag={gh} school="Kings University College" status="stopped"/></li>
+                    </ul>
+                </Records>
+                 <Buttons />
+            </RecordsContaner>
         </Container>
     )
 }
---
+++ src/utils/strings/education/institiutions.tsx
@@ -0,0 +1,156 @@
+export const fieldset1 = [
+    {
+        fieldType: "radio",
+        label: "Nationality",
+        id: "01",
+        Radio: [
+            {
+                value: "Ghana",
+                id1: "Ghana",
+                id2: "GhanaLabel",
+                label: "Ghana",
+                id: "01",
+            },
+            {
+                value: "International",
+                id1: "International",
+                id2: "InternationalLabel",
+                label: "International",
+                id: "02",
+            },
+        ]
+    },
+];
+
+export const fieldset2 = [
+       {
+    value: "",
+    id: "01",
+    fieldType: "select",
+        label: "From",
+    width: "70%",
+    options: [{
+      value: "2019",
+      content: " 2019",
+      id: "01",
+    },
+        {
+      value: "2023",
+      content: " 2023",
+      id: "02",
+    }
+        ],
+  },
+       {
+    value: "",
+    id: "02",
+    fieldType: "select",
+        label: "To",
+    width: "70%",
+    options: [{
+      value: "2023",
+      content: " 2023",
+      id: "01",
+    },
+        {
+      value: "2022",
+      content: " 2022",
+      id: "02",
+    }
+        ],
+  },
+       {
+    value: "",
+    id: "03",
+    fieldType: "select",
+        label: "District",
+    width: "100%",
+    options: [{
+      value: "Ahanta west",
+      content: "Ahanta west",
+      id: "01",
+    },
+        {
+      value: "Ahanta East",
+      content: " Ahanta East",
+      id: "02",
+    }
+        ],
+  },
+]
+
+export const fieldset3 = [
+    {
+        label: "Institution",
+        value: "",
+        placeholder: "Ahantaman Girls S.H.S",
+        inputType: "text",
+        fieldType: "input",
+        id: "01",
+        width: "100%",
+    },
+]
+
+export const fieldset4 = [
+       {
+    value: "",
+    id: "01",
+    fieldType: "select",
+        label: "Level",
+    width: "95%",
+    options: [{
+      value: "Senior Hight",
+      content: " Senior Hight",
+      id: "01",
+    },
+        {
+      value: "Tertiary",
+      content: "Tertiary",
+      id: "02",
+    }
+        ],
+  },
+       {
+    value: "",
+    id: "02",
+    fieldType: "select",
+        label: "Official Exams",
+    width: "95%",
+    options: [{
+      value: "WASCE",
+      content: "WASCE",
+      id: "01",
+    },
+        {
+      value: "SSCE",
+      content: " SSCE",
+      id: "02",
+    }
+        ],
+  },
+]
+export const fieldset5 = [
+       {
+    value: "",
+    id: "01",
+    fieldType: "select",
+        label: "Status",
+    width: "70%",
+    options: [{
+      value: "Completed",
+      content: "Completed",
+      id: "01",
+    },
+        {
+      value: "Ongoing",
+      content: "Ongoing",
+      id: "02",
+    },
+        {
+      value: "Stopped",
+      content: "Stopped",
+      id: "03",
+    },
+        ],
+  },
+]
